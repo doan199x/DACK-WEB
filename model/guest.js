832fc301777ed5199667243b990dfa88424f0050
X\ADMIN <nttdap123@gmail.com>
2020-12-15 06:00:46 +0700

    Model example, update readme

---
+++ model/guest.js
@@ -0,0 +1,9 @@
+const db = require('../utils/db');
+
+//module.export = {
+    //name: async (input) => {
+        //const sql = `your sql command` ${nameofobject}
+        //const result = await db.load(sql);
+        //return result;
+    //},
+//};
