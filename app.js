3f7832a5b1e0d2bab0cc6e1a3c9ad833ee4f23dc
X\ADMIN <nttdap123@gmail.com>
2020-12-15 18:58:03 +0700

    Upđate app.js, add error.hbs, add main.hbs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 const exphbs = require('express-handlebars');
 var createError = require('http-errors');
+var path = require ('path');
+
 
 const hbs = exphbs.create({
     defaultLayout: 'main',
@@ -23,21 +25,19 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + '/public')));
 
-app.use('/', require('./router/guest.js'));
-app.use('/admin', require('./router/admin.js'));
-app.use('/student', require('./router/student.js'));
-app.use('/lecturer', require('./router/lecturer.js'));
+app.use('/', require('./routers/guest'));
+app.use('/admin', require('./routers/admin'));
+app.use('/student', require('./routers/student'));
+app.use('/lecturer', require('./routers/lecturer'));
 
 app.use(function (req, res, next) {
     next(createError(404));
 });
 app.use(function (err, req, res, next) {
-
     res.locals.message = err.message;
     res.locals.error = err;
-
     res.status(err.status || 500);
     res.render('error');
 });
