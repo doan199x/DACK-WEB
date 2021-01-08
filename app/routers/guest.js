179d7e70051273a1290044287bb998bf2dfcce96
X\ADMIN <nttdap123@gmail.com>
2021-01-08 18:59:31 +0700

    Modify layouts

--- app/routers/guest.js
+++ app/routers/guest.js
@@ -1,24 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const guestModel = require('../model/guest.js')
+const guestModel = require("../model/guest.js");
 // const modelname = require('../models/modelname');
 
-router.get('/', async (req, res) => {
-    const top = await  guestModel.top();
-    const topview =  await guestModel.topview();
-    const newest=  await guestModel.newest();
-    const mostwatched =  await guestModel.mostwatched();
-    res.render('home', {
-        css: [''],
-        js: [''],
-        contain: 'guest/guest',
-        title: 'Home',
-        top: top,
-        topview: topview,
-        newest:newest,
-        mostwatched: mostwatched
+router.get("/", async (req, res) => {
+  try {
+    const top = await guestModel.top();
+    const topview = await guestModel.topview();
+    const newest = await guestModel.newest();
+    const mostwatched = await guestModel.mostwatched();
+    res.render("home", {
+      css: ["guest"],
+      js: [""],
+      contain: "guest/guest",
+      title: "Home",
+      top: top,
+      topview: topview,
+      newest: newest,
+      mostwatched: mostwatched,
     });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
