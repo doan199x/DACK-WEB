038ddc441068f3504f2e690311e4ecd7c35f6824
X\ADMIN <nttdap123@gmail.com>
2021-01-14 04:40:39 +0700

    Course, full-text search

--- app/routers/course.js
+++ app/routers/course.js
@@ -1,21 +1,40 @@
 const express = require("express");
 const course = require("../model/course.js");
 const router = express.Router();
+const helper = require("../helper/pagination");
 
 const courseModel = require("../model/course.js");
 
 router.get("/", async (req, res) => {
+  if (req.query.page == null || req.query.page.trim() == "") {
+    req.query.page = 2;
+  }
+  if (req.query.perPage == null || req.query.perPage.trim() == "") {
+    req.query.perPage = 5;
+  }
   const all = await courseModel.all();
+  for (var i = 0; i < all.length; i++) {
+    all[i].widthStar = all[i].averageStar / 5 * 100;
+    all[i].widthStar += '%';
+}
+  var page = req.query.page;
+  var perPage = req.query.perPage;
+  
+  const pagingInfo = helper.pagination(all,page,perPage,all.length);
+  // add width star
+ 
   try {
     res.render("home", {
-      css: ["course"],
-      js: [""],
+      css: ["course","rate"],
+      js: ["course"],
       contain: "course/course",
-      all: all,
+      courses: pagingInfo.objectOnPage,
+      pagingInfo: pagingInfo,
+      currentPage: page,
+      perPage: perPage
     });
   } catch (err) {
     console.log(err);
   }
 });
-
 module.exports = router;
