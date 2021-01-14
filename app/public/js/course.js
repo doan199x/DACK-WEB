bae787de26db02e635d12bc42cc56bd887ea39c9
X\ADMIN <nttdap123@gmail.com>
2021-01-14 16:27:38 +0700

    Full text search UI

--- app/public/js/course.js
+++ app/public/js/course.js
@@ -5,3 +5,8 @@ function getPrevPage(preve_value, perPage) {
 function getNextPage(next_value, perPage) {
     location.href = `/course?page=${next_value}&perPage=${perPage}`;
 }
+
+function searchCourse(){
+    const search = document.getElementById('search').value;
+    location.href=`/course/find?search=${search}`;
+}
--- app/routers/course.js
+++ app/routers/course.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const course = require("../model/course.js");
 const router = express.Router();
 const helper = require("../helper/pagination");
-
 const courseModel = require("../model/course.js");
 
 router.get("/", async (req, res) => {
@@ -14,27 +12,64 @@ router.get("/", async (req, res) => {
   }
   const all = await courseModel.all();
   for (var i = 0; i < all.length; i++) {
-    all[i].widthStar = all[i].averageStar / 5 * 100;
-    all[i].widthStar += '%';
-}
+    all[i].widthStar = (all[i].averageStar / 5) * 100;
+    all[i].widthStar += "%";
+  }
   var page = req.query.page;
   var perPage = req.query.perPage;
-  
-  const pagingInfo = helper.pagination(all,page,perPage,all.length);
+
+  const pagingInfo = helper.pagination(all, page, perPage, all.length);
   // add width star
- 
+
   try {
     res.render("home", {
-      css: ["course","rate"],
+      css: ["course", "rate"],
       js: ["course"],
       contain: "course/course",
       courses: pagingInfo.objectOnPage,
       pagingInfo: pagingInfo,
       currentPage: page,
-      perPage: perPage
+      perPage: perPage,
     });
   } catch (err) {
     console.log(err);
   }
 });
+
+router.get("/find", async (req, res) => {
+  try {
+    let courses;
+    if (req.query.page == null || req.query.page.trim() == "") {
+      req.query.page = 1;
+    }
+    if (req.query.perPage == null || req.query.perPage.trim() == "") {
+      req.query.perPage = 3;
+    }
+    if (req.query.search === null || req.query.search.trim() === "") {
+      courses = await courseModel.getAll();
+    } else {
+      courses = await courseModel.fulltext(req.query.search);
+    }
+    // add width star
+    for (var i = 0; i < courses.length; i++) {
+      courses[i].widthStar = (courses[i].averageStar / 5) * 100;
+      courses[i].widthStar += "%";
+    }
+    var page = req.query.page;
+    var perPage = req.query.perPage;
+    var pagingInfo = helper.pagination(courses, page, perPage, courses.length);
+    res.render("home", {
+      css: ["course", "rate"],
+      js: ["course"],
+      contain: "course/course",
+      courses: pagingInfo.objectOnPage,
+      pagingInfo: pagingInfo,
+      currentPage: page,
+      perPage: perPage,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
